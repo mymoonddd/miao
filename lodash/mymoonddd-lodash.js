0f42cb26cefd5696d6c34c42e16f82d4c26f49dc
Shirley <xiaochunyan0218@126.com>
2022-01-10 17:24:27 +0800

    修改

--- lodash/mymoonddd-lodash.js
+++ lodash/mymoonddd-lodash.js
@@ -1,6 +1,84 @@
-var  <mymoonddd> = {
-  chunk: function(array, size) {
+var mymoonddd = function() {
 
+  function chunk(array, size = 1) {
+      let result = []
+      for (let i = 0; i < array.length;) {
+          let group = []
+          for (let j = 0; j < size; j++) {
+              group.push(array[i])
+              i++
+              if (i == array.length) {
+                  break
+              }
+          }
+          result.push(group)
+      }
+      return result
   }
 
-}
+  function compact(array) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i]) {
+            result.push(array[i])
+        }
+    }
+    return result
+  }
+
+  function drop(array, n = 1) {
+      let result = []
+      if (n < array.length) {
+          for (let i = n; i < array.length; i++) {
+              if (array[i]) {
+                  result.push(array[i])
+              }
+          }
+      }
+      return result
+  }
+
+  function dropRight(array, n = 1) {
+      let result = []
+      let len = array.length
+      if (n < len) {
+          for (let i = 0; i < len - n ; i++) {
+              if (array[i]) {
+                  result.push(array[i])
+              }
+          }
+      }
+      return result
+  }
+
+  function fill(array, value, start = 0, end = array.length) {
+      for (let i = start; i < end; i++) {
+          array[i] = value
+      }
+      return array
+  }
+
+  function flatten(array) {
+      let result = []
+      for (let i = 0; i < array.length; i++) {
+          if (Array.isArray(array[i])) {
+              for (let j = 0; j < array[i].length; j++) {
+                  result.push(array[i][j])
+              }
+          } else {
+              result.push(array[i])
+          }
+      }
+      return result
+  }
+
+  return {
+    chunk: chunk,
+    compact: compact,
+    drop: drop,
+    dropRight: dropRight,
+    fill: fill,
+    flatten: flatten,
+  }
+
+}()
