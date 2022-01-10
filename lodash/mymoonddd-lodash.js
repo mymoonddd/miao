93a6801a993fd43f0707d74c082c5d60f9f803eb
Shirley <xiaochunyan0218@126.com>
2022-01-10 22:50:04 +0800

    修改

--- lodash/mymoonddd-lodash.js
+++ lodash/mymoonddd-lodash.js
@@ -107,7 +107,7 @@ var mymoonddd = function() {
       if (i == array.length - 1) {
         result += array[i]
       } else {
-        result += array[i] + separator
+        result += "" + array[i] + separator
       }
     }
     return result
@@ -157,6 +157,120 @@ var mymoonddd = function() {
     return result
   }
 
+  function zip() {
+    let n = arguments.length
+    let m = arguments[0].length
+    let result = Array(m)
+    for (let i = 0; i < m; i++) {
+      let inner = []
+      for (let j = 0; j < n; j++) {
+        inner[j] = arguments[j][i]
+      }
+      result[i] = inner
+    }
+    return result
+  }
+
+  function sample(collection) {
+    let random = Infinity
+    while (random > collection.length - 1) {
+      random = Math.floor(10 * Math.random())
+    }
+    return collection[random]
+  }
+
+  function size(collection) {
+    if (typeof(collection) == "boolean") {
+      return 0
+    }
+    if (typeof(collection) == "number") {
+      return 0
+    }
+    if (typeof(collection) == "string") {
+      let i = 0
+      while (collection[i]) {
+        i++
+      }
+      return i
+    }
+    if (Array.isArray(collection)) {
+      return collection.length
+    }
+    if (typeof(collection) == "object") {
+      let count = 0
+      for (let i in collection) {
+        count ++
+      }
+      return count
+    }
+  }
+
+  function isBoolean(value) {
+    return value === true || value === false
+  }
+
+  function ceil(number, precision=0) {
+    let x = 10 ** ( -precision )
+    let remain = number % x
+    if (!remain) {
+      return number
+    }
+    return (number - remain) + x
+  }
+
+  function max(array) {
+    let max = array[0]
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i]
+      }
+    }
+    return max
+  }
+
+  function sum(array) {
+    let sum = array[0]
+    for (let i = 1; i < array.length; i++) {
+        sum += array[i]
+    }
+    return sum
+  }
+
+  function repeat(string='', n=1) {
+    if (!n) {
+      return ''
+    }
+    let result = string
+    for (let i = 1; i < n; i++) {
+      result += string
+    }
+    return result
+  }
+
+  function range(start=0, end, step=1) {
+    let result = []
+    if (step) {
+      let len = (end - start) / step
+    } else {
+      len = end - start
+    }
+    if (arguments.length == 1) {
+      end = start
+      start = 0
+      if (end < 0) {
+        step = -1
+      }
+    }
+
+    if (len > 0) {
+      for (let count = 0, i = start; count < len; count++, i += step) {
+        result.push(i)
+      }
+    }
+    return result
+  }
+
+
   return {
     chunk: chunk,
     compact: compact,
@@ -174,6 +288,12 @@ var mymoonddd = function() {
     reverse: reverse,
     uniq: uniq,
     without: without,
-  }
-
+    zip: zip,
+    size: size,
+    isBoolean: isBoolean,
+    ceil: ceil,
+    max: max,
+    sum: sum,
+    repeat: repeat,
+    range: range,
 }()
