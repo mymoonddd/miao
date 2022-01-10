135c425775d6ef976174ff12db2a81b03615ed84
Shirley <xiaochunyan0218@126.com>
2022-01-10 19:56:54 +0800

    修改

--- lodash/mymoonddd-lodash.js
+++ lodash/mymoonddd-lodash.js
@@ -72,6 +72,91 @@ var mymoonddd = function() {
       return result
   }
 
+  function fromPairs(pairs) {
+    let result = {}
+    for (let i = 0; i < pairs.length; i++) {
+        result[pairs[i][0]] = pairs[i][1]
+    }
+    return result
+  }
+
+  function head(array) {
+    return array[0]
+  }
+
+  function indexOf(array, value, fromIndex=0) {
+    if (fromIndex < 0) {
+      fromIndex += array.length
+    }
+    for (let i = fromIndex; i < array.length; i++) {
+      if (array[i] == value) {
+        return i
+      }
+    }
+    return -1
+  }
+
+  function initial(array) {
+    array.length -= 1
+    return array
+  }
+
+  function join(array, separator=',') {
+    let result = ""
+    for (let i = 0; i < array.length; i++) {
+      if (i == array.length - 1) {
+        result += array[i]
+      } else {
+        result += array[i] + separator
+      }
+    }
+    return result
+  }
+
+  function last(array) {
+    return array[array.length - 1]
+  }
+
+  function lastIndexOf(array, value, fromIndex=array.length-1) {
+    for (let i = fromIndex; i >= 0; i--) {
+      if (array[i] == value) {
+        return i
+      }
+    }
+    return -1
+  }
+
+  function reverse(array) {
+    let n = array.length
+    let result = Array(n)
+    for (let i = 0; i < n; i++) {
+      result[n - i - 1] = array[i]
+    }
+    return result
+  }
+
+  function uniq(array) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+      let p = array[i]
+      if (!(result.includes(p))) {
+        result.push(p)
+      }
+    }
+    return result
+  }
+
+  function without(array, ...values) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+      let p = array[i]
+      if (!values.includes(p)) {
+        result.push(p)
+      }
+    }
+    return result
+  }
+
   return {
     chunk: chunk,
     compact: compact,
@@ -79,6 +164,16 @@ var mymoonddd = function() {
     dropRight: dropRight,
     fill: fill,
     flatten: flatten,
+    fromPairs: fromPairs,
+    head: head,
+    indexOf: indexOf,
+    initial: initial,
+    join: join,
+    last: last,
+    lastIndexOf: lastIndexOf,
+    reverse: reverse,
+    uniq: uniq,
+    without: without,
   }
 
 }()
